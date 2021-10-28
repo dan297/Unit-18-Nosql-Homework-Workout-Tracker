7180a6450e97372d850147981beb34e1fdd14b1f
Dan <Danielwhite98@icloud.com>
2021-10-28 14:15:46 +0100

    deploying

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts.js");
+
+//addExercise
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    .then((dbRes) => {
+      res.json(dbRes);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+//createWorkout
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbRes) => {
+      res.json(dbRes);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+module.exports = router;
